7cccd2e6aa86b3ed5ea073d6fc1da45a98932dec
Foebry <rain_fabry@hotmail.com>
2022-02-25 10:10:14 +0100

    display tiles on screen

--- js/index.js
+++ js/index.js
@@ -7,10 +7,16 @@ class Tile {
 		this.transition_speed = speed;
 		this.holder = holder;
 		this.element = this.generateHTML();
+		this.setStyling();
 	}
 	generateHTML() {
 		this.holder.insertAdjacentHTML("beforeend", `<div class="tile"></div>`);
 		return this.holder.querySelector(".tile:last-child");
+	}
+	setStyling() {
+		this.bgc = `rgb(${[...Array(3).fill(0).map(el=>Math.round(Math.random() * 255))]})`;
+		this.element.style.backgroundColor = this.bgc;
+		this.element.style.border = `2px solid ${this.bgc}`;
 	}
 }
 
